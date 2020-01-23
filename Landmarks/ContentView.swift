9f93e552152be448ddcb5083ab080ad77547967a
Alex Shillingford <alex.shillingford@icloud.com>
2020-01-22 19:28:17 -0700

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,33 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Alex Shillingford on 1/21/20.
+//  Copyright © 2020 Alex Shillingford. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text("Turtle Rock")
+                .font(.title)
+            
+            HStack {
+                Text("Joshua Tree National Park")
+                    .font(.subheadline)
+                Spacer()
+                Text("California")
+                    .font(.subheadline)
+            }
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
